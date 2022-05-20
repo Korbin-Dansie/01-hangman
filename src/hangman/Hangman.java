a2fdd4c42839ad1eed4effaa8a871ffadde6b5c9
Korbin-Dansie <Dansiekm@gmail.com>
2022-05-20 15:13:21 -0600

    first

---
+++ src/hangman/Hangman.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package hangman;
+
+/**
+ * @author Korbin Dansie
+ *
+ */
+
+public class Hangman {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub a
+		System.out.println("Hello");
+
+	}
+
+}
